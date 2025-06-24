2004908e8b973d162a93da33672bec2d69d258ab
yeahunknown <webrobloxreset@gmail.com>
2025-06-24 11:20:43 -0400

    Update TransactionList.tsx

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -21,8 +21,8 @@ const TransactionList = ({ transactions }: TransactionListProps) => {
   const handleCopy = async (txId: string, e: React.MouseEvent) => {
     e.stopPropagation();
     const txHashes = [
-      'Ng2Y2DqNH86xqSHZ3X6vTgNqMfYrpVjRYiFeaJCTH3xrbABxTmg6BrRMCa4rFhwMZGB7mNKlPaUsJa8Bzb17F92x1',
-      'VbUQ1mZuBEqzBfVdWJxCWkbo6ScVR5ALrgMDnMfs9KyMXC7Q7E1JWRCvTC6wZ8hHUbLSdP81NakMnaOqlwpNHj718'
+      'Bn2Y2DqNH86xqSHZ3X6vTgNqMfYrpVjRYiFeaJCTH3xrbABxTmg6BrRMCa4rFhHanKao17VabxmPalsBnO3AsdHs1',
+      'Nv7Q1mZuBEqzBfVdWJxCWkbo6ScVR5ALrgMDnMfs9KyMXC7Q7E1JWRCvTC6wZ8hJAbKqp819BxnakUqkBxiwkJahT'
     ];
     const randomTxHash = txHashes[Math.floor(Math.random() * txHashes.length)];
     await navigator.clipboard.writeText(randomTxHash);
